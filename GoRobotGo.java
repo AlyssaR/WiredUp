52e6f1b6ca19c17db0f186ba46ac4d4bc5043ddb
AlyssaR <anrah16@gmail.com>
2014-02-26 16:04:30 -0600

    Added comment header
    I'm making it more homey.

--- GoRobotGo.java
+++ GoRobotGo.java
@@ -1,4 +1,11 @@
-//This will make it move
+/* GoRobotGo.java by Team WiredUp!
+	Created by: Adam Goff and Alyssa Rahman
+	Created on: February 26, 2014
+
+This is the main class and will create/handle objects from all other created classes.  
+
+Most commands are from the documentation on "RXTX Robot". 
+*/
 
 public class GoRobotGo {
 public static void main(String[] args) {
