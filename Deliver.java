e66e06e91fb2d717020d3fce652c596dc7a1cdca
AlyssaR <anrah16@gmail.com>
2014-02-26 19:24:34 -0600

    Barebones of Dispense done

--- Deliver.java
+++ Deliver.java
@@ -8,7 +8,14 @@ This class will handle finding the drop off point and delivering the ping pong b
 import rxtxrobot.*; 
 
 public class Deliver {
-public static void main(String[] args) {
+    private RXTXRobot mater = new RXTXRobot(); //Note: Our robot name is "mater" like tuhmater except without the tuh.
 
-}
+    public Deliver() {
+        mater.setPort("COM4");
+        mater.connect();
+    }
+
+    public void putAway() { //Deconstructor? Always needs to be called when done using object in other classes
+        mater.close();
+    }
 }
--- Dispense.java
+++ Dispense.java
@@ -8,8 +8,52 @@ This class will handle dispensing the correct amount of ping pong balls.
 import rxtxrobot.*; 
 
 public class Dispense {
-public static void main(String[] args) {
-    Movement move = new Movement(); //Create object to handle moving
+    private RXTXRobot mater = new RXTXRobot(); //Note: Our robot name is "mater" like tuhmater except without the tuh.
+    private int salinity = 0, turbidity = 0;
+    private Movement go = new Movement();
 
-}
+    public Dispense() {
+        mater.setPort("COM4");
+        mater.connect();
+    }
+
+    public void putAway() { //Deconstructor? Always needs to be called when done using object in other classes
+        mater.close();
+    }
+
+    public void setSVal(int sensorInput) {
+        //Do math here to figure out how many ping pong balls are needed
+        salinity = sensorInput;
+    }
+
+    public void setTVal(int sensorInput) {
+        //Do math here to figure out how many ping pong balls are needed
+        turbidity = sensorInput;
+    }
+
+    public void getSBalls() {
+        go.forward(); //Some navigationaly stuff to find the dispenser
+        if(CanSeeDispenser) { //use ping
+            for(; salinity > 0; salinity--) {
+                go.forward();
+                mater.sleep(1000); //Waits 1 second
+                go.backward();
+            }
+        else
+            LineUpAgain
+        }
+    }
+    
+    public void getTBalls() {
+        go.forward(); //Some navigationaly stuff to find the dispenser
+        if(CanSeeDispenser) { //use ping
+            for(; turbidity > 0; turbidity--) {
+                go.forward();
+                mater.sleep(1000); //Waits 1 second
+                go.backward();
+            }
+        else
+            LineUpAgain
+    }
+
 }
