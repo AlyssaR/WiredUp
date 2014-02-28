b8000994538b2bcf48de227fcd06fbe64817f606
acgoff <acgoff@smu.edu>
2014-02-28 17:24:52 -0600

    desired change to dispense
    we don't need the math yet

--- Dispense.java
+++ Dispense.java
@@ -9,8 +9,9 @@ import rxtxrobot.*;
 
 public class Dispense {
     RXTXRobot mater = new RXTXRobot(); //Note: Our robot name is "mater" like tuhmater except without the tuh.
-    double sBalls, leftS, rightS, tBalls, leftT, rightT;
-    double salinReading = 0, turbidReading = 0;
+    float sNeeded, leftS, rightS, tNeeded, leftT, rightT;
+    float salinReading = 0, turbidReading = 0;
+    int PingPongBalls = 0;
     Movement go;
 
     public Dispense(float salin, float turbid, String port) {
@@ -37,6 +38,48 @@ public class Dispense {
                 mater.sleep(1000); //Waits 1 second
                 go.move(SPEED, DISTANCE); //Backward
             }
+        /*  My method of getting balls. takes into acount the which one is higher.
+            Mesures the amount of ping pong balls held.
+        //Salinity Method
+            if (leftS > rightS){
+                while (sNeeded >= leftS){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    sNeeded -= leftS;
+                    heldPingPongBalls++;
+                }
+                while (sNeeded > 0){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    sNeeded -= rightS;
+                    heldPingPongBalls++;
+                }
+            }
+
+            else{
+                while (sNeeded >= rightS){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    sNeeded -= rightS;
+                    heldPingPongBalls++;
+                }
+                while (sNeeded > 0){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    sNeeded -= leftS;
+                    heldPingPongBalls++;
+                }
+            }
+
+        */
         }
         else {
             //LineUpAgain
@@ -51,6 +94,46 @@ public class Dispense {
                 mater.sleep(1000); //Waits 1 second
                 go.move(SPEED, DISTANCE); //Backward
             }
+            /*
+            //turbidity method
+            if (leftT > rightT){
+                while (tNeeded >= leftT){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    tNeeded -= leftT;
+                    heldPingPongBalls++;
+                }
+                while (tNeeded > 0){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    tNeeded -= rightT;
+                    heldPingPongBalls++;
+                }
+            }
+
+            else{
+                while (tNeeded >= rightT){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    tNeeded -= rightT;
+                    heldPingPongBalls++;
+                }
+                while (tNeeded > 0){
+                    go.move(SPEED, DISTANCE); //Forward
+                    mater.sleep(1000); //Waits 1 second
+                    go.move(SPEED, DISTANCE); //Backward
+
+                    tNeeded -= leftT;
+                    heldPingPongBalls++;
+                }
+            }
+             */
         }
         else {
             //LineUpAgain
